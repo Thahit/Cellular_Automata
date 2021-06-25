e25edbcd80fa463ed627ebe9c04abbf706574059
Thahit <you@example.comnicolas.blumer@gmail.com>
2021-06-25 19:18:50 +0200

    adjusted array size to 30
    Signed-off-by: Thahit <you@example.comnicolas.blumer@gmail.com>

--- Experiment1/main.cpp
+++ Experiment1/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
+const int size = 30;
+
 template<typename T, size_t n>
-void printArray(T const(& arr)[n])
-{
-    for (size_t i = 0; i < n; i++) {
+void printArray(T const(& arr)[n]){
+    for (size_t i = 0; i < size; i++) {
         std::cout << arr[i] << ' ';
     }
     std::cout << "\n";
 }
 
 int * step(int *arr){   
-    int n = sizeof(arr)/sizeof(arr[0]);
-    static int output [8]={0,0,0,0,0,0,0,0};
-    for (size_t i = 0; i < n; i++) {
+    static int output [size]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    for (size_t i = 0; i < size; i++) {
         int neighbours =0;
         for (int y = 0; y < 3; y++){
-            int neigh = (i-1+y) %n;
+            int neigh = (i-1+y) %size;
             if ( arr[neigh]==1){
                 neighbours +=1;
             }
@@ -31,18 +31,17 @@ int * step(int *arr){
 }
 
 int main(){
-    int world [8] = {1,1,1,0,1,0,1,1};
+    int world [size] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     printArray(world);
     int *pWorld = step(world);
     for (size_t round = 0; round < 20; round++)
     {
         int *pWorld = step(pWorld);
 
-        for (size_t i = 0; i < 8; i++) {
+        for (size_t i = 0; i < size; i++) {
             std::cout << pWorld[i] << ' ';
         }
         std::cout << '\n';
     }
-    
     return 0;
 }
