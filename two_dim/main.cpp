4180ffecf34778a8703aecba1fd50ac4befaff7c
Thahit <nicolas.blumer@gmail.com>
2022-09-09 12:33:29 +0200

    game of live

--- two_dim/main.cpp
+++ two_dim/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <unistd.h>
 
-const int size = 24;//board size
+const int size = 32;//board size
+const int radius = 1;//perception field of a cell
 
 void show_board(int board[size][size]){
+    /**
+     * prints the gamestate
+     * 
+     * @param board = the gamestate to be visualized
+     */
     int len = size*2+3;
 
     for(int i = 0; i<len; i++){//border
@@ -30,35 +36,42 @@ void show_board(int board[size][size]){
 
 
 int rule_set(int own_state, int num_alive){
+    /**
+     * ruleset of the original "game of life"
+     * 
+     * @param own_state = 1 if the sell is alive or 0 if it's dead
+     * @param num_alive = number of neighbours who are alive
+     * 
+     * @return 1 if cell is alive in the next timestep and 0 if it is not
+     */
     if((own_state == 0 && num_alive==2) || num_alive ==3){
         return 1;
     }
     return 0;
 }
 
-int** step(int board[size][size]){
+void step(int board[size][size]){
     /**
      * Updates the board according to set rules
      * 
      * @param board[size][size] = input board to be updated
-     * 
      */
     int num_alive;
-    int** new_board[size][size];// need a 2. board to not mess with having new and old cells at the same time
+    int temp_board[size][size];
+    std::copy(&board[0][0], &board[0][0]+size*size,&temp_board[0][0]);// need a 2. board to not mess with having new and old cells at the same time
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             // count alive neighbours
-            num_alive = 0 - board[i][j];// because the own state is also looked at when counting neighbours
-            for(int k = -1; k<2; k++){
-                for(int l = -1; l<2; l++){
-                    num_alive += board[(i+k)%size][(j+l)%size];//if we would go out of the board we reenter on the other side
+            num_alive = 0 - temp_board[i][j];// because the own state is also looked at when counting neighbours
+            for(int k = -radius; k<=radius; k++){
+                for(int l = -radius; l<=radius; l++){
+                    num_alive += temp_board[(i+k)%size][(j+l)%size];//if we would go out of the board we reenter on the other side
                 }
             }
 
-            new_board[i][j] = rule_set(board[i][j], num_alive);
+            board[i][j] = rule_set(temp_board[i][j], num_alive);
         }
     }
-    return new_board;
 }
 
 void play(int board[size][size], int num_iter){
@@ -67,17 +80,15 @@ void play(int board[size][size], int num_iter){
      * 
      * @param board[size][size] = board to play with
      * @param num_iter = number of iterations
-     * 
      */
     show_board(board);//show init board
 
     for(int i = 0; i < num_iter; i++){
-        step(board);
-
-        show_board(board);
-
         // wait
-        sleep(2);
+        sleep(1);
+
+        step(board);
+        show_board(board);
     }
 }
 
@@ -87,7 +98,6 @@ void fill_random(int board[size][size], float frac){
      * 
      * @param board[size][size] = input board to be filled
      * @param frac = the approximate fraction of spaces to be filled with alive cells
-     * 
      */
     float random_number;
 
@@ -104,8 +114,8 @@ void fill_random(int board[size][size], float frac){
 int main(){
     int board[size][size];
 
-    fill_random(board, .1);
+    fill_random(board, .05);
 
-    play(board, 3);
+    play(board, 100);
     return 0;
 }
